b4eace3d2868f5f11c69cd948e35982251e20f63
yluiz <luizvictor1231@gmail.com>
2023-05-23 21:03:43 -0400

    redeploy

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   const port = process.env.PORT || 4000;
-  await app.listen(port);
+  await app.listen(port, "0.0.0.0");
 }
 bootstrap();
